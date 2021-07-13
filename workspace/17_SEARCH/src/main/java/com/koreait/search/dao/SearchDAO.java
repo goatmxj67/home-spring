ea76a73ff7d098c6fffea3ea8135f1f0d4cb5b6c
goatmxj67 <yulki67@naver.com>
2021-07-13 12:55:26 +0900

    update 2021-07-13

---
+++ workspace/17_SEARCH/src/main/java/com/koreait/search/dao/SearchDAO.java
@@ -0,0 +1,17 @@
+package com.koreait.search.dao;
+
+import java.util.List;
+
+import com.koreait.search.dto.Employees;
+import com.koreait.search.dto.PageDTO;
+import com.koreait.search.dto.QueryDTO;
+
+public interface SearchDAO {
+
+	public int getTotalRecord();
+	public List<Employees> searchAll(PageDTO pageDTO);
+	public List<Employees> autoComplete(QueryDTO queryDTO);
+	public List<Employees> search(QueryDTO queryDTO);
+	public int getSearchRecord(QueryDTO queryDTO);
+	
+}
